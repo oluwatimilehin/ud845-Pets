62f1ef49ca36276f205d1422fc7a3ccb0a16148f
Oluwatimilehin <timad97@Yahoo.com>
2017-01-21 22:42:34 +0100

    Define insert method

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -9,6 +9,8 @@ import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import com.example.android.pets.data.PetContract.PetEntry;
 
+import static android.R.attr.id;
+
 /**
  * Created by timad on 21/01/2017.
  */
@@ -90,7 +92,27 @@ public class PetProvider extends ContentProvider {
      */
     @Override
     public Uri insert(Uri uri, ContentValues contentValues) {
-        return null;
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PETS:
+                return insertPet(uri, contentValues);
+            default:
+                throw new IllegalArgumentException("Insertion is not supported for " + uri);
+        }
+    }
+
+    /**
+     * Insert a pet into the database with the given content values. Return the new content URI
+     * for that specific row in the database.
+     */
+    private Uri insertPet(Uri uri, ContentValues values) {
+
+        // TODO: Insert a new pet into the pets database table with the given ContentValues
+        SQLiteDatabase database = mPetDbHelper.getWritableDatabase();
+        database.insert(PetEntry.TABLE_NAME, null, values);
+        // Once we know the ID of the new row in the table,
+        // return the new URI with the ID appended to the end of it
+        return ContentUris.withAppendedId(uri, id);
     }
 
     /**
