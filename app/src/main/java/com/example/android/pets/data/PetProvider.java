d5018317c9bfd102025097f3e77b4fb7e2247778
Oluwatimilehin <timad97@Yahoo.com>
2017-01-21 19:43:57 +0100

    Initialize PetDebHelper object in content provider

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -11,6 +11,8 @@ import android.net.Uri;
 
 public class PetProvider extends ContentProvider {
 
+    public PetDbHelper mPetDbHelper;
+
     /** Tag for the log messages */
     public static final String LOG_TAG = PetProvider.class.getSimpleName();
 
@@ -22,6 +24,7 @@ public class PetProvider extends ContentProvider {
         // TODO: Create and initialize a PetDbHelper object to gain access to the pets database.
         // Make sure the variable is a global variable, so it can be referenced from other
         // ContentProvider methods.
+        mPetDbHelper = new PetDbHelper(getContext());
         return true;
     }
 
