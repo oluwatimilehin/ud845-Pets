b52da13edd65d23dad5bf66fbfbf987ea599ad38
Oluwatimilehin <timad97@Yahoo.com>
2017-01-22 21:36:23 +0100

    Set delete all pets menu option

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -62,6 +62,9 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
 
     }
 
+    private void deleteAllPets(){
+        getContentResolver().delete(PetEntry.CONTENT_URI, null, null);
+    }
     private void insertPet(){
         ContentValues values = new ContentValues();
         values.put(PetEntry.COLUMN_PET_NAME,"Toto");
@@ -92,7 +95,7 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
                 return true;
             // Respond to a click on the "Delete all entries" menu option
             case R.id.action_delete_all_entries:
-                // Do nothing for now
+                deleteAllPets();
                 return true;
         }
         return super.onOptionsItemSelected(item);
